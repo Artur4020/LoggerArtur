393125629e680dd095459ca3be88f32a795c5bbb
Artur4020 <75090645+Artur4020@users.noreply.github.com>
2020-11-26 21:53:47 +0200

    Add files via upload

---
+++ LoggerArtur/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LoggerArtur
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
